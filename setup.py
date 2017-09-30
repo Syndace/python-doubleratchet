bc42ad48498c623da30560a5601cba2c5da57274
Tim Henkes <tim.henkes@student.hs-rm.de>
2017-09-30 18:27:25 +0200

    Addes setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = "DoubleRatchet",
+    version = "0.1",
+    description = "A python implementation of the Double Ratchet algorithm.",
+    author = "Tim Henkes",
+    url = "https://github.com/Syndace/python-doubleratchet",
+    packages = ["doubleratchet", "doubleratchet.chains", "doubleratchet.exceptions", "doubleratchet.implementations", "doubleratchet.ratchets"],
+    requires = ["scci", "hkdf"],
+    provides = ["doubleratchet"]
+)
