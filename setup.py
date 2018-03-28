d572f4c19fe7114cc493dec9fd9cf7e4a48a0ae2
Tim Henkes <tim.henkes@student.hs-rm.de>
2018-03-28 13:03:59 +0200

    Versioning scheme changed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name = "DoubleRatchet",
-    version = "0.1",
+    version = "0.1.0",
     description = "A python implementation of the Double Ratchet algorithm.",
     author = "Tim Henkes",
     url = "https://github.com/Syndace/python-doubleratchet",
