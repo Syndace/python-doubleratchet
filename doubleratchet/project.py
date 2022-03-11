6bcdd8ae2af2af046ed21a229398010fd0bbc3a9
Tim Henkes <tim.henkes@hs-rm.de>
2022-03-11 11:50:29 +0100

    Copyright year bump

--- doubleratchet/project.py
+++ doubleratchet/project.py
@@ -2,7 +2,7 @@ project = {
     "name"         : "DoubleRatchet",
     "description"  : "A Python implementation of the Double Ratchet algorithm.",
     "url"          : "https://github.com/Syndace/python-doubleratchet",
-    "year"         : "2020",
+    "year"         : "2022",
     "author"       : "Tim Henkes (Syndace)",
     "author_email" : "me@syndace.dev",
     "categories"   : [
