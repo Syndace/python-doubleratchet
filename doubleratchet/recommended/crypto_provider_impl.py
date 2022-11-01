5774c4db3f43f2aba28298aed2df4942ceaaf7cc
Tim Henkes <me@syndace.dev>
2022-11-01 20:40:18 +0100

    Added Brython-based CryptoProvider and use it if available.

--- doubleratchet/recommended/crypto_provider_impl.py
+++ doubleratchet/recommended/crypto_provider_impl.py
@@ -1,4 +1,13 @@
-from .crypto_provider_cryptography import CryptoProviderImpl
+from typing import Type
+
+from .crypto_provider import CryptoProvider
+
+
+CryptoProviderImpl: Type[CryptoProvider]
+try:
+    from .crypto_provider_brython import CryptoProviderImpl
+except ImportError:
+    from .crypto_provider_cryptography import CryptoProviderImpl
 
 
 __all__ = [
